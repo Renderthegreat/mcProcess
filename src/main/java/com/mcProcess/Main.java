3d4fd104a7e4a9f44df91b67249fd534e63f7cef
Renderthegreat <renderthegreat.business@gmail.com>
2024-02-19 02:27:55 +0000

    Update

--- src/main/java/com/mcProcess/Main.java
+++ src/main/java/com/mcProcess/Main.java
@@ -1,4 +1,5 @@
 package com.mcProcess;
+
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,52 +13,57 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main extends JavaPlugin {
-    
-    
-    @Override
-    public void onEnable() {
-        try{
-            Process process = Runtime.getRuntime().exec("node ../javascript/index.js");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        getLogger().info("mcP is Starting...");
-        
-        
-        FileConfiguration config = loadConfig();
-        if (config.contains("id")) {
-            String id = config.getString("id");
-            readFromPipe("mcProcess#" + id);
-        }
-    }
 
-    @Override
-    public void onDisable() {
-        getLogger().info("mcP has been disabled.");
-    }
+	@Override
 
-    private FileConfiguration loadConfig() {
-        Path configFile = Paths.get(getDataFolder().toString(), "mcP/pipe.yml");
-        if (!Files.exists(configFile)) {
-            saveResource("pipe.yml", false);
-        }
-        return YamlConfiguration.loadConfiguration(configFile.toFile());
-    }
+	public void onEnable() {
+		getLogger().info("mcP is Starting...");
 
-    private void readFromPipe(String pipePath) {
-        new Thread(() -> {
-            try {
-                FileInputStream fis = new FileInputStream(pipePath);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    getLogger().info("Received from named pipe: " + line);
-                }
-                reader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }).start();
-    }
+		try {
+			ProcessBuilder processBuilder = new ProcessBuilder("node", "../javascript/index.js");
+			Process process = processBuilder.start();
+			int exitCode = process.waitFor();
+			if (exitCode == 0) {
+				getLogger().info("Index.js script executed successfully.");
+			} else {
+				getLogger().warning("Node.js script execution failed with exit code: " + exitCode);
+			}
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+		FileConfiguration config = loadConfig();
+		if (config.contains("id")) {
+			String id = config.getString("id");
+			readFromPipe("mcProcess#" + id);
+		}
+	}
+
+	@Override
+	public void onDisable() {
+		getLogger().info("mcP has been disabled.");
+	}
+
+	private FileConfiguration loadConfig() {
+		Path configFile = Paths.get(getDataFolder().toString(), "mcP/pipe.yml");
+		if (!Files.exists(configFile)) {
+			saveResource("pipe.yml", false);
+		}
+		return YamlConfiguration.loadConfiguration(configFile.toFile());
+	}
+
+	private void readFromPipe(String pipePath) {
+		new Thread(() -> {
+			try {
+				FileInputStream fis = new FileInputStream(pipePath);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					getLogger().info("Received from named pipe: " + line);
+				}
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}).start();
+	}
 }
-
